f79c9d07c2ca7a4594dfb35747f934a17744d0c6
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-08 16:24:14 +0200

    helpers functions added in bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,11 @@
 //load config file
 require_once "config/config.php";
 
+//load helpers
+require_once "helpers/sessionHelper.php";
+require_once "helpers/urlHelper.php";
+
+
 // load libraries automatically
 spl_autoload_register(function ($className) {
     require_once "libraries/$className.php";
